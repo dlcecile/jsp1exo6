06ca226575189195b588e91783f8736a4b1360c7
DL <deleucecile@outlook.fr>
2019-10-23 14:29:27 +0200

    fin exo 6

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+function control()
+{
+  var saisie1=document.getElementById("firstNumber").value;
+  var saisie2=document.getElementById("secondNumber").value;
+  result= saisie1%saisie2
+  alert(`Résultat: ${result});
+}
